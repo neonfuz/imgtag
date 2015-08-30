5bcb9da11df6138749302fc40c5e8e0fba4f45e1
Sage Raflik <neonfuz@CherisefliksAir.att.net>
2015-08-30 15:41:35 -0500

    Made program work with multiple files

--- prog.c
+++ prog.c
@@ -14,28 +14,33 @@
     return retval;				\
   } while (0);
 
-
 int main(int argc, char **argv)
 {
-  if (argc != 2)
-    err(1, "Error, please specify exactly one file.\n");
+  if (argc < 2)
+    err(1, "Error, please specify files as arguments.\n");
 
-  int fildes = open(argv[1], O_RDONLY);
-  if (fildes < 0)
-    err(2, "Error opening \"%s\"\n", argv[1]);
+  int arg;
+  for(arg=1; arg<argc; ++arg) {
+    int fildes = open(argv[arg], O_RDONLY);
+    if (fildes < 0)
+      err(2, "Error opening \"%s\".\n", argv[arg]);
+    
+    struct stat st;
+    
+    if (fstat(fildes, &st) < 0)
+      err(3, "Error stating file \"%s\"\n", argv[arg]);
+    
+    char *file = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fildes, 0);
+    
+    if (file == MAP_FAILED)
+      err(4, "Error mapping file \"%s\" to memory\n", argv[arg]);
+    
+    printf("%s: 0x%" PRIx32 "\n", argv[arg], crc32(0, file, st.st_size));
+    printf("insert into test (filename, crc32) values(%s, %"PRIu32");\n", argv[arg], crc32(0, file, st.st_size));
 
-  struct stat st;
-
-  if (fstat(fildes, &st) < 0)
-    err(3, "Error stating file \"%s\"\n", argv[1]);
-
-  char *file = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fildes, 0);
-
-  if (file == MAP_FAILED)
-    err(4, "Error mapping file to memory\n");
-
-  printf("%s: 0x%" PRIx32 "\n", argv[1], crc32(0, file, st.st_size));
-  printf("insert into test (filename, crc32) values(%s, %"PRIu32");\n", argv[1], crc32(0, file, st.st_size));
+    munmap(file, st.st_size);
+    close(fildes);
+  }
 
   return 0;
 }
