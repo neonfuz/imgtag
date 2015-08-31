bd49de26f4f20df836d4176a3fb22a9aab6783ae
Sage Raflik <neonfuz@gmail.com>
2015-08-30 19:14:51 -0500

    Made it so program doesn't exit prematurely when it encounters an error with one of the files it is processing

--- prog.c
+++ prog.c
@@ -12,7 +12,7 @@
 #define err(retval, format, ...)				\
 	do {										\
 		fprintf(stderr, format, ##__VA_ARGS__);	\
-		return retval;							\
+		continue;								\
 	} while (0);
 
 #define warn(format, ...) fprintf(stderr, format, ##__VA_ARGS__)
@@ -25,19 +25,27 @@ int main(int argc, char **argv)
 	int arg;
 	for(arg=1; arg<argc; ++arg) {
 		int fildes = open(argv[arg], O_RDONLY);
-		if (fildes < 0)
-			err(2, "Error opening \"%s\".\n", argv[arg]);
-    
+		if (fildes < 0) {
+			warn("Error opening \"%s\".\n", argv[arg]);
+			continue;
+		}
+
 		struct stat st;
     
-		if (fstat(fildes, &st) < 0)
-			err(3, "Error stating file \"%s\"\n", argv[arg]);
-    
+		if (fstat(fildes, &st) < 0) {
+			warn("Error stating file \"%s\"\n", argv[arg]);
+			close(fildes);
+			continue;
+		}
+		
 		char *file = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fildes, 0);
     
-		if (file == MAP_FAILED)
-			err(4, "Error mapping file \"%s\" to memory\n", argv[arg]);
-    
+		if (file == MAP_FAILED) {
+			warn("Error mapping file \"%s\" to memory\n", argv[arg]);
+			close(fildes);
+			continue;
+		}
+		
 		//    printf("%s: 0x%" PRIx32 "\n", argv[arg], crc32(0, file, st.st_size));
 		printf("insert into test (filename, crc32) values(\"%s\", %"PRIu32");\n", argv[arg], crc32(0, file, st.st_size));
 
